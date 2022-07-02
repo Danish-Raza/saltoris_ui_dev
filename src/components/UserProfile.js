0d6943e8759c19631c85db49cc780ba46c9d9d7e
Danish Raza <danishraza@Vipuls-MacBook-Pro-2.local>
2022-07-02 20:27:36 +0530

    updating branch

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,6 @@
 import { Fragment, useState } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
 import { logout } from "../actions/appActions";
 import Icon from "../Icon";
 
@@ -7,6 +8,7 @@ function UserProfile(props) {
     const { userDetail } = props;
     const [dropDownStatus, setDropDownStatus] = useState(false)
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     document.querySelector("body").addEventListener("click", (e) => {
         var element = document.querySelector('.user-profile-section');
         if(!element.contains(e.target)) {
@@ -14,7 +16,10 @@ function UserProfile(props) {
         }
 
     })
-
+    const handleLogout = () => {
+        navigate("/")
+        dispatch(logout())
+    }
     return (
         <Fragment>
             <div className="profile-section" onClick={() => setDropDownStatus(!dropDownStatus)}>
@@ -35,7 +40,7 @@ function UserProfile(props) {
                         <div className="option">
                             {userDetail.email}
                         </div>
-                        <div className="option logout-option" onClick={() =>  dispatch(logout())}>
+                        <div className="option logout-option" onClick={handleLogout}>
                           Logout
                         </div>
                     </div>
