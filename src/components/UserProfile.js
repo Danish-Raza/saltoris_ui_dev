9ecfea68f2a8a587cf115dda55aa813630c89a31
Danish Raza <danishraza@Vipuls-MacBook-Pro-2.local>
2022-07-02 20:30:19 +0530

    updating branch

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,19 +1,26 @@
 import { Fragment, useState } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
 import { logout } from "../actions/appActions";
 import Icon from "../Icon";
 
 function UserProfile(props) {
     const { userDetail } = props;
-    const [dropDownStatus, setDropDownStatus] = useState(false)
-    const dispatch = useDispatch()
+    const [dropDownStatus, setDropDownStatus] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     document.querySelector("body").addEventListener("click", (e) => {
         var element = document.querySelector('.user-profile-section');
         if(!element.contains(e.target)) {
-            setDropDownStatus(false)
+            setDropDownStatus(false);
         }
-
     })
+
+    const handleLogout = () => {
+        navigate("/");
+        dispatch(logout());
+    }
 
     return (
         <Fragment>
@@ -35,15 +42,13 @@ function UserProfile(props) {
                         <div className="option">
                             {userDetail.email}
                         </div>
-                        <div className="option logout-option" onClick={() =>  dispatch(logout())}>
+                        <div className="option logout-option" onClick={handleLogout}>
                           Logout
                         </div>
                     </div>
                 )
             }
         </Fragment>
-
     )
-    
 }
-export default UserProfile
+export default UserProfile;
